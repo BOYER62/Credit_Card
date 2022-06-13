ba13ca6920ac63eb035525b4a13a265fd3776f33
BOYER62 <boyer.olivier2@free.fr>
2022-06-13 13:41:46 +0200

    Probleme lien

--- Asset/Header.inc.php
+++
@@ -1,7 +0,0 @@
-
-<h1>
-    Paiement par carte de crédit
-</h1>
-<h2>
-    Frais de transaction <span>0.00 €</span>
-</h2>
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once('Asset/Head.inc.html');
-include_once('Asset/Header.inc.php');
-include_once('Asset/Body.inc.php');
-include_once('Asset/Footer.inc.php');
