0baf848265174e355527352b710fb02838c4388e
BOYER62 <boyer.olivier2@free.fr>
2022-06-14 21:30:13 +0200

    Correction Github

---
+++ Asset/Footer.inc.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="Asset/css/Footer.css">
+<div class="container">
+    <div class="row">
+        <div class="col-6 securite">
+            <h3>
+                Paiement sécurisé par by HiPay SAS, 256-bit SSL
+            </h3>
+        </div>
+        <div class="col-6">
+            <h3 class="paiement">
+                Payer
+            </h3>
+        </div>
+    </div>
+</div>
+</html>
